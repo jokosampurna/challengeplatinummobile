81c43dadcdbf31ab138350b4573affee68d2b6d3
erlynaqabinar <erlynaaudyas@gmail.com>
2022-11-02 22:37:22 +0700

    Edit profile

---
+++ Object Repository/Edit Profile/click nama.rs
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>click nama</name>
+   <tag></tag>
+   <elementGuidId>8faaabd6-5089-46ce-9ed0-2d76840e0190</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator></locator>
+   <locatorStrategy>ATTRIBUTES</locatorStrategy>
+</MobileElementEntity>
